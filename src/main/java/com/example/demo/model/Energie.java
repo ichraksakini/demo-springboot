eebdff58e9f6e1dba91ce2ce4c72fa9f3555fc71
ichraksakini <ichraksakini2001@gmail.com>
2026-04-06 11:52:49 +0200

    add energie fields

--- src/main/java/com/example/demo/model/Energie.java
+++ src/main/java/com/example/demo/model/Energie.java
@@ -10,7 +10,25 @@ public class Energie {
     @Column(name = "id")
     private String id;
 
+    @Column(name = "date")
+    private String date;
+
+    @Column(name = "heure_debut")
+    private String heure_debut;
+
+    @Column(name = "technicien")
+    private String technicien;
+
+    @Column(name = "gasoil_citerne_1")
+    private String gasoil1;
+
+    @Column(name = "gasoil_citerne_2")
+    private String gasoil2;
+
+    // 🔥 Constructeur obligatoire
     public Energie() {}
+
+    // 🔥 Getters & Setters
 
     public String getId() {
         return id;
@@ -18,5 +36,45 @@ public class Energie {
 
     public void setId(String id) {
         this.id = id;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getHeure_debut() {
+        return heure_debut;
+    }
+
+    public void setHeure_debut(String heure_debut) {
+        this.heure_debut = heure_debut;
+    }
+
+    public String getTechnicien() {
+        return technicien;
+    }
+
+    public void setTechnicien(String technicien) {
+        this.technicien = technicien;
+    }
+
+    public String getGasoil1() {
+        return gasoil1;
+    }
+
+    public void setGasoil1(String gasoil1) {
+        this.gasoil1 = gasoil1;
+    }
+
+    public String getGasoil2() {
+        return gasoil2;
+    }
+
+    public void setGasoil2(String gasoil2) {
+        this.gasoil2 = gasoil2;
     }
 }
